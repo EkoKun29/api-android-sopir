10a0536a4f50ea54c2b48f205f0f4ff5fbf17f10
EkoKun29 <ekoseptyonugroho9@gmail.com>
2025-02-08 21:32:19 +0700

    fix:api

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
+
 
 class Handler extends ExceptionHandler
 {
@@ -45,4 +47,10 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+{
+    return response()->json(['message' => 'Unauthenticated.'], 401);
+}
+
 }
