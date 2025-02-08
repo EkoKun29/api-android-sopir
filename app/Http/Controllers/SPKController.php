7d075da2d71fbb7235249e01de1c1dd8c8fd4445
EkoKun29 <ekoseptyonugroho9@gmail.com>
2025-02-08 15:25:12 +0700

    fix:api

--- app/Http/Controllers/SPKController.php
+++ app/Http/Controllers/SPKController.php
@@ -12,26 +12,31 @@ class SPKController extends Controller
    
     public function store(Request $request)
 {
-    $user = Auth::user(); 
-    if (!$user) {
-        return response()->json(['error' => 'User tidak ditemukan'], 401);
+    try {
+        $user = Auth::user(); 
+        if (!$user) {
+            return response()->json(['error' => 'User tidak ditemukan'], 401);
+        }
+
+        $spk = SPK::create([
+            'id_user' => $user->id, // Ambil ID user dari token login
+            'tanggal' => Carbon::now()->format('d/m/Y'),
+            'nama_sales' => $request->nama_sales,
+            'tanggal_muat' => Carbon::createFromFormat('j/n/Y', $request->tanggal_muat)->format('d/m/Y'),
+            'hari_jam_keberangkatan' => $request->hari_jam_keberangkatan,
+            'hari_Jam_kepulangan' => $request->hari_Jam_kepulangan,
+            'sopir' => $request->sopir,
+            'rute' => $request->rute,
+            'dropper' => $request->dropper,
+            'keterangan' => $request->keterangan,
+        ]);
+
+        return response()->json($spk, 201);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
     }
-
-    $spk = SPK::create([
-        'id_user' => $user->id, // Ambil ID user dari token login
-        'tanggal' => Carbon::now()->format('d/m/Y'),
-        'nama_sales' => $request->nama_sales,
-        'tanggal_muat' => Carbon::parse($request->tanggal_muat)->format('d/m/Y'),
-        'hari_jam_keberangkatan' => $request->hari_jam_keberangkatan,
-        'hari_Jam_kepulangan' => $request->hari_Jam_kepulangan,
-        'sopir' => $request->sopir,
-        'rute' => $request->rute,
-        'dropper' => $request->dropper,
-        'keterangan' => $request->keterangan,
-    ]);
-
-    return response()->json($spk, 201);
 }
+
 
 
 
