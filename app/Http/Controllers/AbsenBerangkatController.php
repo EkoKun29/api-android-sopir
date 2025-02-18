4610e58ad1260abb8dfbf5c0a7b19404458268b2
EkoKun29 <ekoseptyonugroho9@gmail.com>
2025-02-18 21:33:07 +0700

    coba

--- app/Http/Controllers/AbsenBerangkatController.php
+++ app/Http/Controllers/AbsenBerangkatController.php
@@ -42,23 +42,46 @@ class AbsenBerangkatController extends Controller
             //     'uuid' => Str::uuid(),
             // ]);
 
-            $absenBerangkat = AbsenBerangkat::create([
-                'id_user' => $user->id,
-                'nama' => $user->name,
-                'jabatan' => $user->role,
-                $fileName = $request->face . '.jpeg', 
-                $imagePath = 'public/' . $fileName, 
-                $image = str_replace('data:image/jpeg;base64,', '', $imageData),
-                $image = str_replace(' ', '+', $image),
-                Storage::put($imagePath, base64_decode($image)),
-                'face' => $fileName, 
-                'tanggal' => Carbon::now()->format('d/m/Y'),
-                'jam' => Carbon::now()->format('H:i:s'),
-                'latitude' => $request->latitude, 
-                'longitude' => $request->longitude,
-                'lokasi' => $request->lokasi, // Pastikan request mengirim 'lokasi' dan bukan '$fileName'
-                'uuid' => Str::uuid(),
-            ]);
+            $absenBerangkat = new AbsenBerangkat();
+            $absenBerangkat ->id_user = Auth::user()->id;
+            $absenBerangkat->uuid =Str::uuid();
+            $absenBerangkat->nama = Auth::user()->name;
+            $absenBerangkat->jabatan = Auth::user()->role;
+            $absenBerangkat->tanggal = Carbon::now()->format('d/m/Y');
+            $absenBerangkat->jam = Carbon::now()->format('H:i:s');
+            $absenBerangkat->latitude = $request->latitude;
+            $absenBerangkat->longitude = $request->longitude;
+            $absenBerangkat->lokasi = $request->lokasi;
+            $fileName = $absenBerangkat->uuid . '.png'; // Nama file
+            $imagePath = 'public/' . $fileName; // Path untuk disimpan di storage
+            $image = str_replace('data:image/png;base64,', '', $imageData);
+            $image = str_replace(' ', '+', $image);
+
+            // Simpan gambar di storage/app/public
+            Storage::put($imagePath, base64_decode($image)); // Simpan gambar
+
+            // Simpan nama file ke database
+            $absenBerangkat->face = $fileName;
+            $absenBerangkat->save();
+
+
+            // $absenBerangkat = AbsenBerangkat::create([
+            //     'id_user' => $user->id,
+            //     'nama' => $user->name,
+            //     'jabatan' => $user->role,
+            //     $fileName = $request->face . '.jpeg', 
+            //     $imagePath = 'public/' . $fileName, 
+            //     $image = str_replace('data:image/jpeg;base64,', '', $imageData),
+            //     $image = str_replace(' ', '+', $image),
+            //     Storage::put($imagePath, base64_decode($image)),
+            //     'face' => $fileName, 
+            //     'tanggal' => Carbon::now()->format('d/m/Y'),
+            //     'jam' => Carbon::now()->format('H:i:s'),
+            //     'latitude' => $request->latitude, 
+            //     'longitude' => $request->longitude,
+            //     'lokasi' => $request->lokasi, // Pastikan request mengirim 'lokasi' dan bukan '$fileName'
+            //     'uuid' => Str::uuid(),
+            // ]);
 
             return response()->json($absenBerangkat, 201);
         } catch (\Exception $e) {
